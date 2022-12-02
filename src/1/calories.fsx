01d5d9c207b86289b07314a719610aafc99dcc44
Adrian Murphy <adrian.murphy99@mail.com>
2022-12-02 18:38:00 +0100

    file structure

--- src/1/calories.fsx
+++ src/1/calories.fsx
@@ -4,4 +4,4 @@ let text: string = File.ReadAllText("in.txt")
 let elves: int list = [for elf: string in text.Trim().Split("\n\n") -> elf.Split("\n") |> Seq.sumBy int]
 
 List.max elves |> printfn "%A" // answer 1
-List.sortDescending elves |> Seq.take 3 |> Seq.sumBy int |> printfn "%A"
+List.sortDescending elves |> Seq.take 3 |> Seq.sumBy int |> printfn "%A" // answer 2
