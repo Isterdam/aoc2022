e378934621619bdb5122e16cb7567fecdd5551b1
Adrian Murphy <adrian.murphy99@mail.com>
2022-12-05 12:10:27 +0100

    refactored

--- src/5/supply.py
+++ src/5/supply.py
@@ -3,22 +3,19 @@ lines = open('in.txt').read().splitlines()
 part1 = False
 stacks = [[] for _ in range(9)]
 
-for line in lines:
-    if '[' not in line:
-        break
-    j = 1
-    for i in range(1, len(line), 4):
-        if line[i] != ' ':
-            stacks[i-j].append(line[i])
-        j += 3
-
 def move(n, fr, to):
     # only reverse the crates if part 1
     stacks[to-1] = stacks[fr-1][0:n][::-1 if part1 else 1] + stacks[to-1]
     stacks[fr-1] = stacks[fr-1][n:]
 
 for line in lines:
-    if 'move' in line:
+    if '[' in line:
+        j = 1
+        for i in range(1, len(line), 4):
+            if line[i] != ' ':
+                stacks[i-j].append(line[i])
+            j += 3
+    elif 'move' in line:
         args = line.split(' ')
         move(int(args[1]), int(args[3]), int(args[5]))
 
