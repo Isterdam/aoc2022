4d343c5214f4037930f7afb098a44b1ba2b15b3b
Adrian Murphy <adrian.murphy99@mail.com>
2022-12-05 16:49:11 +0100

    more general parser

--- src/5/supply.py
+++ src/5/supply.py
@@ -1,22 +1,20 @@
 lines = open('in.txt').read().splitlines()
 
 part1 = False
-stacks = [[] for _ in range(9)]
+stacks = [[] for _ in range(len(lines[0][1::4]))]
 
 def move(n, fr, to):
     # only reverse the crates if part 1
-    stacks[to-1] = stacks[fr-1][0:n][::-1 if part1 else 1] + stacks[to-1]
-    stacks[fr-1] = stacks[fr-1][n:]
+    stacks[to] = stacks[fr][0:n][::-1 if part1 else 1] + stacks[to]
+    stacks[fr] = stacks[fr][n:]
 
 for line in lines:
     if '[' in line:
-        j = 1
-        for i in range(1, len(line), 4):
-            if line[i] != ' ':
-                stacks[i-j].append(line[i])
-            j += 3
+        crates = line[1::4]
+        for i in range(len(crates)):
+            if crates[i] != ' ': stacks[i].append(crates[i])
     elif 'move' in line:
-        args = line.split(' ')
-        move(int(args[1]), int(args[3]), int(args[5]))
+        args = line.split()
+        move(int(args[1]), int(args[3])-1, int(args[5])-1)
 
 print(''.join([stack[0] for stack in stacks]))
