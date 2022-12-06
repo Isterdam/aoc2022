f9e4092928761356128867119bc48fec1bb9dae3
Adrian Murphy <adrian.murphy99@mail.com>
2022-12-06 11:18:05 +0100

    slightly cleaner

--- src/6/tuning.py
+++ src/6/tuning.py
@@ -1,4 +1,4 @@
 line = open('in.txt').read().strip()
 
-print([i+4 for i in range(len(line)-4) if len(set(line[i:i+4]))==4][0]) # answer 1
-print([i+14 for i in range(len(line)-14) if len(set(line[i:i+14]))==14][0]) # answer 2
+for j in [4, 14]: # answer 1 and 2
+    print([i+j for i in range(len(line)-j) if len(set(line[i:i+j]))==j][0])
