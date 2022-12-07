06be67c4b283c5e3014dfe562a6c75aba6d1063c
Adrian Murphy <adrian.murphy99@mail.com>
2022-12-07 18:26:39 +0100

    refactored day 7

--- src/7/dirs.py
+++ src/7/dirs.py
@@ -1,51 +1,32 @@
-lines = open('in.txt').read().splitlines() + ['$']
+from collections import defaultdict
+import itertools
 
-dirs = {}
+def def_value():
+    return []
+
+lines = open('in.txt').read().splitlines()
+
+dirs = defaultdict(def_value)
 curr_dir = []
-while len(lines) > 0:
-    line = lines[0]
+for line in lines:
     dir = '/'.join(curr_dir)
 
-    if '$ cd ..' in line:
+    if '$ cd ..' == line:
         curr_dir.pop() # pop dir from stack
     elif '$ cd' in line:
         curr_dir.append(line.split()[2]) # push current dir to stack
     elif '$ ls' in line:
-        lines = lines[1:]
-        contained_files = []
-
-        while True: # parse all the files inside current dir
-            line = lines[0]
-            if 'dir' in line:
-                contained_files.append(dir+'/'+line.split()[1])
-            else:
-                contained_files.append(line.split()[0])
-            if '$' in lines[1]: # hence + ['$'] to lines on row 1
-                break
-            lines = lines[1:]
-
-        dirs[dir] = contained_files
-
-    lines = lines[1:]
+        continue
+    else:
+        if 'dir' in line:
+            dirs[dir].append(dir+'/'+line.split()[1])
+        else:
+            dirs[dir].append(line.split()[0])
 
 for k in dirs.keys():
     while '/' in ''.join(dirs[k]): # while dir contains other dirs
-        subdirs = []
-        for v in dirs[k]:
-            if v in dirs.keys():
-                subdirs += dirs[v]
-            else:
-                subdirs += [v]
-        dirs[k] = subdirs
-
-sought_dirs_size = 0
-deleted_dir_size = 70000000
-for k in dirs.keys():
+        dirs[k] = list(itertools.chain(*[dirs[v] if v in dirs.keys() else [v] for v in dirs[k]]))
     dirs[k] = sum(list(map(int, dirs[k])))
-    if dirs[k] <= 100000:
-        sought_dirs_size += dirs[k]
-    if 70000000 - dirs['/'] + dirs[k] >= 30000000:
-        deleted_dir_size = min(deleted_dir_size, dirs[k])
 
-print(sought_dirs_size) # answer 1
-print(deleted_dir_size) # answer 2
+print(sum([v for v in dirs.values() if v <= 100000])) # answer 1
+print(min([v for v in dirs.values() if 70000000 - dirs['/'] + v >= 30000000])) # answer 2
